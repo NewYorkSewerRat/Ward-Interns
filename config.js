37722d23a358a3c9c7887342ee96015e862fa5bd
NewYorkSewerRat <sting40000@gmail.com>
2026-09-24 21:35:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // Use the "anon public" key ONLY. Never paste the secret (service) key here.
 window.WB_CONFIG = {
   SUPABASE_URL: "https://ecelirutxjoirkboxccn.supabase.co/rest/v1/",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjZWxpcnV0eGpvaXJrYm94Y2NuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyOTI0MjMsImV4cCI6MjEwNTg2ODQyM30.iatXIR-hpiVrfRf0saF8ZIVxsMxLizvzAQF72lxDfO4",
+  SUPABASE_ANON_KEY: "sb_publishable_vbvZINXHZcLCJKTnyiSs6w_C4672y0O",
   IDLE_MINUTES: 15   // signs out after this many minutes without activity
 };
